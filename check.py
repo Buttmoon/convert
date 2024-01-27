f43cb91bdc581389dd3e9ae70746d57bc72fbdad
Buttmoon <lonokil48@gmail.com>
2024-01-27 13:07:50 +0300

    first commit

---
+++ check.py
@@ -0,0 +1,50 @@
+import os
+import json
+
+
+def netscapeToJson(filename):
+    x = "Cookies//" + filename
+    myfile = open(x, "r")
+    cookie = []
+    myline = myfile.readlines()
+    for line in myline:
+        key = {}
+        x = line.split()
+        try:
+            key['domain'] = x[0]
+            key['httpOnly'] = x[1] == "TRUE"
+            key['path'] = x[2]
+            key['secure'] = x[3] == "TRUE"
+
+            key['expirationDate'] = int(x[4])
+            key['name'] = x[5]
+            key['value'] = x[6]
+            cookie.append(key)
+        except:
+            continue
+    myfile.close()
+    filepath = path + filename
+    file1 = open(filepath, 'w')
+    file1.writelines(str(json.dumps(cookie, indent=2)))
+    file1.close()
+
+
+if __name__ == "__main__":
+
+    path = 'cookies_json//'
+    try:
+        os.mkdir(path)
+    except OSError as error:
+        print(error)
+
+    # Get the list of all files and directories
+    pathc = "Cookies"
+    dir_list = os.listdir(pathc)
+
+    print("TASK STARTED...")
+
+    for h in dir_list:
+        print("new file", h)
+        netscapeToJson(h)
+
+    print("TASK COMPLETED...")
